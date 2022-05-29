2c45ca788c5f3f26603ada6f049bfccc4c11b955
MansalapusJennelyn <mansalapusjennelyn@gmail.com>
2022-05-29 11:45:37 -0700

    index added

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Your Career Suiting Guide</title>
+        <link rel="icon" type="image/x-icon" href="img/icon2.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+            <a href="index.php">
+                <img src="img/logo2.png" width="200" height="70"> </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="shs.php">SHS Track and Strand</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">About Us</a></li>                         
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Header-->
+        <header class="text-muted">
+            <div class="container px-4 text-center">
+
+<head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+    border: solid orange;
+    padding: 10px;
+}
+
+h2  {text-align: center;
+    font-size: 20px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+}
+h3  {text-align: left;
+    font-size: 30px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: blacks;
+}
+</style>
+</head>
+
+<body>
+<h1> Your Career Suiting Guide</h1>                
+<h2> You Dream, We Provide</h2>
+
+</html>
+<style type="text/css">
+    header{ background-image: url('img/bg.gif');
+    background-repeat: no-repeat; background-size:  100% 100%;}
+
+</style>
+</body>         
+</div>
+        </header>
+        <!-- About section-->
+        <section id="home">  
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content">
+                    <div class="col-lg-12 text-justify">
+                        
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  flex: 100%;
+  padding: 25px;
+  height: auto; 
+  border: 10px solid gray;
+}
+</style>
+
+<body>
+
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    <?php
+    echo "<p> Congratulations! You are now almost done with your Junior High School years, and all of us here in Y.C.S.G know that you are still quite confused about which track and which particular strand to take in your Senior High School year. That is why; we created this website for you to be guided and for you to know more about the K-12 program of the Department of Education.
+    Before you choose the track or strand that is right for you, you must first know what K-12 is for and the difference between Academic Tracks, Strands, and Specializations.
+    </p>";
+
+     ?>
+  </div>
+  
+</div>
+</body>
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+
+.column {
+  flex: 100%;
+  padding: 25px;
+  height: auto; 
+  border: 10px solid gray;
+}
+</style>
+
+<body>
+
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    <h3> What is K-12? </h3>
+    <?php
+    echo "<p> K to 12 (also K-12) is an education system under the Department of Education that aims to enhance learners’ basic skills, produce more competent citizens, and prepare graduates for lifelong learning and employment. “K” stands for Kindergarten and “12” refers to the succeeding 12 years of basic education (6 years of elementary education, 4 years of junior high school, and 2 years of senior high school).
+    </p>"
+
+     ?>
+  </div>
+  
+</div>
+</body>
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  flex: 100%;
+  padding: 25px;
+  height: auto; 
+  border: 10px solid gray;
+}
+</style>
+
+<body>
+
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    <h3> SHS TRACK AND STRAND </h3>
+    <?php
+    echo "<p> Students have four general programs or tracks to choose from. Under each program, there are various specializations or strands to specialize in namely: Academic Track, Tech-Voc Track, Sports Track and Arts and Design Track.
+
+    It aims to equip you with knowledge and skills that will help you prepare better for your chosen path be it higher education, employment, or entrepreneurship.
+    </p>"
+
+     ?>
+  </div>
+  
+</div>
+</body>
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Alright Reserves: Y.C.S.G-Your Career Suiting Guide </p></div>
+        </footer>      
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
