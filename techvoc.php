3ae0165d07c9666c9d1edb4f16922bfade41005c
MansalapusJennelyn <mansalapusjennelyn@gmail.com>
2022-05-29 11:56:54 -0700

    techvoc tracks added

---
+++ techvoc.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title> TECHNICAL-VOCATIONAL LIVELIHOOD (TVL) TRACK</title>
+        <link rel="icon" type="image/x-icon" href="img/icon2.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+            <a href="index.php">
+                <img src="img/logo2.png" width="200" height="70"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="shs.php">SHS Track and Strand</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">About Us</a></li>                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="text-muted">
+            <div class="container px-4 text-center">
+
+<head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+    border: solid orange;
+    padding: 10px;
+    text-border-color: black;
+}
+
+h2  {text-align: center;
+    font-size: 30px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: black;
+    border: solid black;
+}
+h3  {text-align: center;
+    font-size: 30px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: black;
+}
+    
+
+
+</style>
+</head>
+<body>
+<h1> Technical Vocational and Livelihood (TVL) Track</h1>                
+
+</html>
+<style type="text/css">
+    header{ background-image: url('img/bg.gif');
+    background-repeat: no-repeat; background-size:  100% 100%;}
+    body{ background-color: gray};
+
+</style>
+</body>
+   
+                
+            </div>
+        </header>
+
+        <section id="shs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-left">
+                    <div class="col-lg-12 text-right">
+                        
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+.row 
+{
+  display: flex;
+}
+.column {
+  flex: 50%;
+  padding: 5px;
+  height: auto; 
+  
+}
+</style>
+
+<body>
+
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+  <img src="img/techvoc.png" width="500" height="500">
+  </div>
+  <div class="column" style="background-color:transparent; height: auto;">
+    <?php
+    echo "<br/>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<h2> TECHNICAL-VOCATIONAL AND LIVELIHOOD (TVL) TRACK </h2>";
+    echo "<br/>";
+    echo "<br/>";
+    echo "<p>If you are really not sure if you will be able to pursue any degree after Senior High or if you want to earn money immediately after graduation, then this track is for you. Even without college, Technical-Vocational-Livelihood track will equip you with job-ready skills in the future.</p>";
+    echo "<br/>";
+    echo "<p>It will gives you the TESDA National Certificate once you finished any of the strands under it that is essential when looking for better career opportunities in agriculture, electronics, and trade. This is also important when applying abroad where the skills you gain would prepare you as you join the workforce. </p>";
+    echo "<br/>";
+    
+     ?>
+  </div>
+  
+</div>
+</body>
+
+<?php 
+        echo "<br/>";
+         ?>
+<section id="shs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: auto; 
+
+}
+</style>
+</head>
+<body>
+
+<div class="row">
+  <h1> STRANDS UNDER TECHNICAL-VOCATIONAL AND LIVELIHOOD (TVL) TRACK </h1>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/agri.png" width="300" height="300">
+      <br>
+      <h3> AGRI-FISHERY ARTS STRAND</h3>
+      <a href="agri.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+      <br>
+      <br>
+      <br>
+      <br>
+      <img src="img/industrial.png" width="300" height="300">
+      <br>
+      <h3> INDUSTRIAL ARTS STRAND </h3>
+      <a href="industrial.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a>    
+  </div>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/he.png" width="300" height="300">
+      <br>
+      <h3> HOME ECONOMICS </h3>     
+      <a href="he.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+      <br>
+      <br>
+      <br>
+      <br>
+      <img src="img/ict.png" width="300" height="300">
+      <br>
+      <h3> INFORMATION AND COMMUNICATION TECHNOLOGY (ICT) STRAND </h3>   
+      <a href="ict.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+  </div>
+</div>
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+.column {
+  flex: 25%;
+  padding: 10px;
+  height: auto; 
+
+}
+</style>
+</head>
+<body>
+
+<div class="row">
+  <h1> OTHER SHS TRACKS</h1>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/techvoc.png" width="300" height="300">
+      <br>
+      <h3> ACADEMIC TRACK </h3>
+      <a href="academic.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a>  
+  </div>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/sports.png" width="300" height="300">
+      <br>
+      <h3> SPORTS TRACK </h3>     
+      <a href="sports.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+  </div>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/arts.png" width="300" height="300">
+      <br>
+      <h3> ARTS AND DESIGN TRACK </h3>
+      <a href="arts.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+  </div>
+</div>
+</body>
+        <?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Alright Reserves: Y.C.S.G-Your Career Suiting Guide </p></div>
+        </footer>      
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
