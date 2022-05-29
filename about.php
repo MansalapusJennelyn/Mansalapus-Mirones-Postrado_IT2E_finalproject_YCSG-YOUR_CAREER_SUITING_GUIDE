c3d3715e2f03fd6a1712b55071d063f8c67eae07
MansalapusJennelyn <mansalapusjennelyn@gmail.com>
2022-05-29 11:52:49 -0700

    about us added

---
+++ about.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>About</title>
+        <link rel="icon" type="image/x-icon" href="img/icon2.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+            <a href="index.php">
+                <img src="img/logo2.png" width="200" height="70"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="shs.php">SHS Track and Strand</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">About</a></li>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="text-muted">
+            <div class="container px-4 text-center">
+
+            <head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+    border: solid orange;
+    padding: 10px;
+}
+
+h2  {text-align: center;
+    font-size: 20px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+}
+h3  {text-align: left;
+    font-size: 30px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: blacks;
+}
+    
+
+
+</style>
+</head>
+<body>
+<h1> Your Career Suiting Guide</h1>                
+<h2> You Dream, We Provide</h2>
+
+</html>
+
+<style type="text/css">
+    header{ background-image: url('img/bg.gif');
+    background-repeat: no-repeat; background-size:  100% 100%;}
+
+</style>
+</body>
+   
+                
+            </div>
+        </header>
+
+        <section id="about">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content">
+                    <div class="col-lg-12 text-justify">
+                        
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  flex: 100%;
+  padding: 25px;
+  height: auto; 
+  border: 10px solid gray;
+}
+</style>
+
+<body>
+<h3> About Us </h3>
+<br/>
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    
+    <?php
+    echo "<p> Y.C.S.G is a privately owned. It is one of the largest online youth platform in the Philippines empowering millions of students aged 13 to 18 to make self-aware education decisions that lead to a fulfilling career and life.
+
+    Students use our website to gain access to the Senior High School strand, Personality Quizzes, Aspiration Quizzes and Field of Interest that enable them to find and take SHS Track and Strand best suited to their own interest, skills, passion and priorities. 
+    
+    </p>"
+
+     ?>
+  </div>
+  
+</div>
+</body>
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  flex: 100%;
+  padding: 25px;
+  height: auto; 
+  border: 10px solid gray;
+}
+</style>
+
+<body>
+<div class="row">
+  <div class="column" style="background-color:transparent; height: auto;">
+    
+    <?php
+    echo "<p> We are the Y. C. S. G team whom concern and want to help some incoming senior highschool students who are afraid to make a choice especially in finding what the best path for them.  We 
+    believe that onceu you choose a strand you like, studying becomes more interesting especially if it is align to your personality and ability.
+    </p>"
+
+     ?>
+  </div>
+  
+</div>
+</body>
+
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Alright Reserves: Y.C.S.G-Your Career Suiting Guide </p></div>
+        </footer>      
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
