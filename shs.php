81de98dbee7d98d823172c539f974f7ee76f1ab8
MansalapusJennelyn <mansalapusjennelyn@gmail.com>
2022-05-29 11:50:34 -0700

    shs added

---
+++ shs.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>SHS Track and Strand</title>
+        <link rel="icon" type="image/x-icon" href="img/icon2.png" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+            <a href="index.php">
+                <img src="img/logo2.png" width="200" height="70"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="shs.php">SHS Track and Strand</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">About Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="text-muted">
+            <div class="container px-4 text-center">
+
+<head>
+<style>
+h1 {text-align: center;
+    font-size: 50px;
+    font-style: italic;
+    font-weight: bold ;
+    color: white;
+    border: solid orange;
+    padding: 10px;
+}
+
+h2  {
+    text-align: left;
+    font-size: 20px ;
+    font-style: italic;
+    font-weight: bold ;
+    color: black;
+}
+h3
+{
+    text-align: center;
+}
+
+</style>
+</head>
+<body>
+<h1> SHS TRACK and STRAND</h1>                
+
+</html>
+<style type="text/css">
+    header{ background-image: url('img/bg.gif');
+    background-repeat: no-repeat; background-size:  100% 100%;}
+
+</style>
+</body>
+   
+                
+            </div>
+        </header>
+
+        <!-- About section-->
+        <section id="shs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+.column {
+  flex: 50%;
+  padding: 1px, 1px;
+  height: auto; 
+
+}
+</style>
+</head>
+<body>
+
+<div class="row">
+  <div class="column" style="background-color:transparent;">
+      <img src="img/academic.png" width="400" height="400">
+      <br>
+      <h3> ACADEMIC TRACK </h3>
+      <h5> College Preparation </h5>
+      <a href="academic.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+      <br>
+      <br>
+      <br>
+      <br>
+      <img src="img/sports.png" width="400" height="400">
+      <br>
+      <h3> SPORTS TRACK </h3>
+      <h5> Physical Education and Recreation </h5>
+      <a href="sports.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a>    
+  </div>
+  <div class="column" style="background-color:transparent;">
+      <img src="img/techvoc.png" width="400" height="400">
+      <br>
+      <h3> TECHNICAL-VOCATIONAL LIVELIHOOD (TVL) TRACK </h3>
+      <h5> Job-ready Skills </h5>
+      <a href="techvoc.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+      <br>
+      <br>
+      <img src="img/arts.png" width="400" height="400">
+      <br>
+      <h3> ARTS AND DESIGN TRACK </h3>   
+      <h5> Design, Perormative and Creative Industry </h5>
+      <a href="arts.php" 
+      style="text-decoration: underlined; color: blue;" > Learn More </a> 
+  </div>
+</div>
+
+
+
+<?php 
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+         ?>
+
+        <footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Alright Reserves: Y.C.S.G-Your Career Suiting Guide </p></div>
+        </footer>      
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
